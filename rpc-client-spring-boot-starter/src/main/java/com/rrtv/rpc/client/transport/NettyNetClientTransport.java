706287eb2639586ffc0112fd2a7c6ee6c6adb3e0
wangchangjiu <wangchangjiu@rr.tv>
2023-05-24 21:53:25 +0800

    Netty 增加连接池

--- rpc-client-spring-boot-starter/src/main/java/com/rrtv/rpc/client/transport/NettyNetClientTransport.java
+++ rpc-client-spring-boot-starter/src/main/java/com/rrtv/rpc/client/transport/NettyNetClientTransport.java
@@ -9,15 +9,13 @@ import com.rrtv.rpc.core.common.RpcResponse;
 import com.rrtv.rpc.core.common.ServiceInfo;
 import com.rrtv.rpc.core.protocol.MessageProtocol;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import lombok.extern.slf4j.Slf4j;
 
+import java.net.InetSocketAddress;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -29,29 +27,10 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class NettyNetClientTransport implements NetClientTransport {
 
-    private final Bootstrap bootstrap;
-    private final EventLoopGroup eventLoopGroup;
-    private final RpcResponseHandler handler;
-
+    private NettyNetClientPool nettyNetClientPool;
 
     public NettyNetClientTransport() {
-        bootstrap = new Bootstrap();
-        eventLoopGroup = new NioEventLoopGroup(4);
-        handler = new RpcResponseHandler();
-        bootstrap.group(eventLoopGroup).channel(NioSocketChannel.class)
-                .handler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel socketChannel) throws Exception {
-                        socketChannel.pipeline()
-
-                                // 解码 是入站操作 将二进制解码成消息
-                                .addLast(new RpcDecoder())
-                                // 接收响应 入站操作
-                                .addLast(handler)
-                                // 编码 是出站操作 将消息编写二进制
-                                .addLast(new RpcEncoder<>());
-                    }
-                });
+        nettyNetClientPool = NettyNetClientPool.getInstance();
     }
 
     @Override
@@ -60,19 +39,11 @@ public class NettyNetClientTransport implements NetClientTransport {
         RpcFuture<MessageProtocol<RpcResponse>> future = new RpcFuture<>();
         LocalRpcResponseCache.add(protocol.getHeader().getRequestId(), future);
 
-        // TCP 连接
-        ChannelFuture channelFuture = bootstrap.connect(metadata.getAddress(), metadata.getPort()).sync();
-        channelFuture.addListener((ChannelFutureListener) arg0 -> {
-            if (channelFuture.isSuccess()) {
-                log.info("connect rpc server {} on port {} success.", metadata.getAddress(), metadata.getPort());
-            } else {
-                log.error("connect rpc server {} on port {} failed.", metadata.getAddress(), metadata.getPort());
-                channelFuture.cause().printStackTrace();
-                eventLoopGroup.shutdownGracefully();
-            }
-        });
-        // 写入数据
-        channelFuture.channel().writeAndFlush(protocol);
+        // 连接池获取 通道 复用通道
+        Channel channel = nettyNetClientPool.getChannel(protocol.getHeader().getRequestId(), metadata.getAddress(), metadata.getPort(), 3);
+        channel.writeAndFlush(protocol);
+
+        nettyNetClientPool.release(channel, metadata.getAddress(), metadata.getPort());
         return metadata.getTimeout() != null ? future.get(metadata.getTimeout(), TimeUnit.MILLISECONDS) : future.get();
     }
 }
